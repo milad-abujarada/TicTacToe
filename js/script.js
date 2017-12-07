ddb5a98720db257e99b0a0873e78497833b42293
miladabujarada <milad.abujarada@gmail.com>
2017-12-06 19:49:47 -0700

    finished the logic of not being able to use the same cell twice

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 	console.log("The DOM is ready");
-	var player = "x";
-	$("div.cell").click( function(){
-		this.append(player);
-		if (player === "x") {
-			player = "o";
+	var player = "X";
+ 	$("div.cell").click( function(){ 
+ 		let $text = $.trim($(this).text());
+ 		console.log($text);
+		if (!($text === "X" || $text === "O")){
+			$(this).append(player);
+			if (player === "X") {
+				player = "O";
+			} else {
+				player = "X";
+			}
 		} else {
-			player = "x";
+				alert("This cell is used please choose anothor cell");
 		}
+
 	});
 	
 });
